7d5a5d93ad92e61ca4717099bd39927f1505debc
Harsh Parihar <pariharharsh0718@gmail.com>
2026-06-10 08:28:56 +0530

    Add files via upload

---
+++ patternThree.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class patternThree {
+    public void pattern3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    static void main() {
+        int N = 10;
+        patternThree sol = new patternThree();
+        sol.pattern3(N);
+    }
+
+
+}
